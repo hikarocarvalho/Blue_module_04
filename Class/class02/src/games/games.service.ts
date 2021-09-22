744e113109c8b582630206a9bf88a7ae9476ef0c
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-09-22 10:26:52 -0300

    update class project

---
+++ Class/class02/src/games/games.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GamesService {
+    findeById(arg0: number) {
+        throw new Error('Method not implemented.');
+    }
+}
