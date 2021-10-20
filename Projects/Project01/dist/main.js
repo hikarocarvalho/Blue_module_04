ccb82c46d2c5984dae1cd705341892ecfa346cc1
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-10-20 19:58:31 -0300

    add errors and add interceptors

--- Projects/Project01/dist/main.js
+++ Projects/Project01/dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const unauthorized_interceptor_1 = require("./interceptors/unauthorized.interceptor");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, {
         cors: {
@@ -14,6 +15,7 @@ async function bootstrap() {
         whitelist: true,
         forbidNonWhitelisted: true,
     }));
+    app.useGlobalInterceptors(new unauthorized_interceptor_1.UnauthorizedInterceptor());
     await app.listen(3001);
 }
 bootstrap();
